4dea523cedb1b27733868b3e8589619e61104603
Sergey <sergey_mikiyanec@mail.ru>
2017-11-20 00:00:15 +0300

    step-5

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,19 +1,19 @@
 'use strict';
 
-describe('phoneList', function() {
+describe('phoneList', function () {
 
-	// Load the module that contains the `phoneList` component before each test
-	beforeEach(module('phoneList'));
+    // Load the module that contains the `phoneList` component before each test
+    beforeEach(module('phoneList'));
 
-	// Test the controller
-	describe('PhoneListController', function() {
+    // Test the controller
+    describe('PhoneListController', function () {
 
-		it('should create a `phones` model with 3 phones', inject(function($componentController) {
-			var ctrl = $componentController('phoneList');
+        it('should create a `phones` model with 3 phones', inject(function ($componentController) {
+            var ctrl = $componentController('phoneList');
 
-			expect(ctrl.phones.length).toBe(3);
-		}));
+            expect(ctrl.phones.length).toBe(3);
+        }));
 
-	});
+    });
 
 });
