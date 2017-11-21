11056a560b2385648719d3bfe6aa26db2030eacc
Sergey <sergey_mikiyanec@mail.ru>
2017-11-21 14:01:47 +0300

    step-7

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,21 +1,10 @@
 angular.module('phoneList').component('phoneList', {
 	templateUrl: 'phone-list/phone-list.template.html',
-	controller: function PhoneListController() {
-		this.phones = [
-			{
-				name: 'Nexus S',
-				snippet: 'Fast just got faster with Nexus S.',
-				age: 4
-			}, {
-				name: 'Motorola XOOM™ with Wi-Fi',
-				snippet: 'The Next, Next Generation tablet.',
-				age: 3
-			}, {
-				name: 'MOTOROLA XOOM™',
-				snippet: 'The Next, Next Generation tablet.',
-				age: 6
-			}
-		];
-		this.orderProp = 'age';
-	}
+	controller: ['$http' , function PhoneListController($http) {
+		var self =this;
+		self.orderProp = 'age';
+		$http.get('phones/phones.json').then(function(response) {
+			self.phones = response.data;
+		});
+	}]
 });
